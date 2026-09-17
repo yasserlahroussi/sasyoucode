34d66cde20ca3cc1709fe79eeb026599e86faa8f
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-17 16:30:08 +0200

    correction

--- day0/ex8.js
+++ day0/ex8.js
@@ -1,7 +1,7 @@
 let angle1 = 90;
-let angle2 = 89;
-let angle3 = 1;
-if ((angle1 != 0 && angle2 != 0 && angle3 != 0) && angle1 + angle2 + angle3 == 180) {
+let angle2 = 91;
+let angle3 = -1;
+if ((angle1 > 0 && angle2 > 0 && angle3 > 0) && angle1 + angle2 + angle3 == 180) {
     console.log("triangle valid ")
 } else {
     console.log("pas de triangle")

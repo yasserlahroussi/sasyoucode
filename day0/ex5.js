e6da52e0cf77b8300e20cab319db7234c7df4bcb
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-17 15:20:26 +0200

    exercice5

---
+++ day0/ex5.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b = 3;
+let c = 4;
+if (a > b && a > c) {
+    console.log(`the largest is ${a}`)
+} else if (b > a && b > c) {
+    console.log(`the largest is ${b}`)
+} else {
+    console.log(`the largest is ${c}`)
+}

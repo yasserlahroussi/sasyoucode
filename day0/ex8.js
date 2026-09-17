564fb2c7749cf91accbdc6e58dbe370dc20a971d
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-17 16:21:37 +0200

    exercice 8 correction

--- day0/ex8.js
+++ day0/ex8.js
@@ -1,10 +1,8 @@
 let angle1 = 90;
 let angle2 = 89;
 let angle3 = 1;
-if (Number(angle1) + Number(angle2) + Number(angle3) == 180) {
-    console.log("triangle valid")
-} else if (angle1 == 0 || angle2 == 0 || angle3 == 0) {
-    console.log("valeur angle null")
+if ((angle1 != 0 && angle2 != 0 && angle3 != 0) && angle1 + angle2 + angle3 == 180) {
+    console.log("triangle valid ")
 } else {
     console.log("pas de triangle")
 }

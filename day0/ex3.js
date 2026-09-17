b2b1f813a9baa0903d5527974c92a81f16ce60ff
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-17 14:43:35 +0200

    modification

--- day0/ex3.js
+++ day0/ex3.js
@@ -3,4 +3,4 @@ if (number % 2 === 0) {
     console.log("even")
 } else {
     console.log("odd")
-};
+}
---
+++ day0/ex4.js
@@ -0,0 +1,12 @@
+let score = 100;
+if (score >= 90 && score <= 100) {
+    console.log("A")
+} else if (score >= 80 && score <= 89) {
+    console.log("B")
+} else if (score >= 70 && score <= 79) {
+    console.log("C")
+} else if (score < 70) {
+    console.log("F")
+} else {
+    console.log("eroor")
+}

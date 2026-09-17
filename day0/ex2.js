e23391e2f3856adcc1499562133d4dbd78b33a50
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-17 14:22:47 +0200

    exex

--- day0/ex2.js
+++ day0/ex2.js
@@ -1,4 +1,4 @@
-let temperature;
+let temperature = 30;
 if (temperature > 30) {
     console.log("It's hot outside!")
 } else {
--- day0/ex3.js
+++ day0/ex3.js
@@ -1,5 +1,5 @@
-let number;
-if (number % 2) {
+let number = 14;
+if (number % 2 === 0) {
     console.log("even")
 } else {
     console.log("odd")

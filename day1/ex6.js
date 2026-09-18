73469cf9f5f848ad350fa8f429921fccf6579ca8
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-18 17:17:04 +0200

    exercice6

---
+++ day1/ex6.js
@@ -0,0 +1,8 @@
+let resultat = "";
+for (let i = 1; i <= 5; i++) {
+    for (j = 1; j <= 5; j++) {
+        resultat = resultat + "* "
+    }
+    resultat = resultat + "\n"
+}
+console.log(resultat);

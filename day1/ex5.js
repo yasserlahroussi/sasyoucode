4545678158aea9a6a94e0083650919ffc3595417
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-18 16:32:20 +0200

    EXERCICE 5

---
+++ day1/ex5.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+console.log("Count Vowels")
+let str = prompt("entrer votre mot : ");
+let count = 0;
+let count1 = 0;
+let count2 = 0;
+for (let i = 0; i < str.length; i++) {
+    if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u") {
+        count++
+    } else if (str[i] == "A" || str[i] == "E" || str[i] == "I" || str[i] == "O" || str[i] == "U") {
+        count1++
+    } else {
+        console.log("eror hhh ")
+    }
+}
+count2 = count + count1
+console.log("num of voyelle is :" + count2)

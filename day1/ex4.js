2d1a04a66ad870b53f1ec3eb6f4bf176270b6a3b
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-18 15:15:09 +0200

    exercice 4

---
+++ day1/ex4.js
@@ -0,0 +1,11 @@
+for (i = 1; i <= 50; i++) {
+    if (i % 3 === 0) {
+        console.log(`${i} est fizz`)
+    } else if (i % 5 === 0) {
+        console.log(`${i} est buzz`)
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz")
+    } else {
+        console.log(i)
+    }
+}

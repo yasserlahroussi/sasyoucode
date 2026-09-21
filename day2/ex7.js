ef091f77c33142bef79e71e55508165c61d85622
yasserlahroussi <yasserlahroussi@gmail.com>
2026-09-21 10:58:07 +0200

    exercice 7

--- day2/ex7.js
+++ day2/ex7.js
@@ -1,6 +1,7 @@
 const p = require('prompt-sync')();
 let a = Number(p("entrer a :"))
 let b = Number(p("entrer b :"))
+let v = p("entrer l'operation :")
 
 
 function calculate(a, b, operation) {
@@ -10,7 +11,11 @@ function calculate(a, b, operation) {
 const add = (a, b) => a + b;
 const soustraction = (a, b) => a - b;
 
+if (v === "+") {
+    console.log(calculate(a, b, add))
 
-
-console.log(calculate(a, b, add))
-console.log(calculate(a, b, soustraction))
+} else if (v === "-") {
+    console.log(calculate(a, b, soustraction))
+} else {
+    console.log("eror")
+}
